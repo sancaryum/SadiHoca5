cbb4b66186c856453fb02594bab049f51989c968
sancaryum <sancaryum@gmail.com>
2020-05-13 00:23:40 +0300

    Files uploaded

---
+++ app/src/main/java/com/example/sadi5/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.sadi5;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText editText;
+    TextView tv;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editText = findViewById(R.id.editTextimiz);
+        tv = findViewById(R.id.textView);
+
+        editText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                tv.setText(editText.getText());
+                editText.setVisibility(View.INVISIBLE);
+                return true;
+            }
+        });
+
+    }
+
+}
